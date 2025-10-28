aa3578fbdfa7d942e3f4766ea1d62cd70190193e
Janne <60547730+Janne6565@users.noreply.github.com>
2025-10-28 13:28:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ const NPM_EXTERNALS: string[] = [];
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === "serve" ? "/" : "/api/ASE-7/",
+  base: command === "serve" ? "/" : "/api/ase-7/",
   plugins: [
     react(),
     command === "serve" && vitePluginReactHMR(ENTRY_POINT),
