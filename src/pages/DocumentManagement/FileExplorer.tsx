f59c2ceb363ee1b9f2b3dc5e336427df273b0960
santino.pia@telekom.de <santino.pia04@gmx.de>
2025-11-06 10:20:11 +0100

    I do not confirm the way of the linter

--- src/pages/DocumentManagement/FileExplorer.tsx
+++ src/pages/DocumentManagement/FileExplorer.tsx
@@ -578,23 +578,23 @@ export default function FileExplorer(): React.ReactElement {
           'success'
         );
       } else if (doc.itemType === 'folder') {
-          const docsForZip = await collectDocsFromFolderWithPaths(
-            doc.id,
-            doc.name
+        const docsForZip = await collectDocsFromFolderWithPaths(
+          doc.id,
+          doc.name
+        );
+
+        if (docsForZip.length === 0) {
+          showSnack(
+            t(
+              'documentManagement.snack.noDocsInSelection',
+              'No documents in folder'
+            ),
+            'error'
           );
+          return;
+        }
 
-          if (docsForZip.length === 0) {
-            showSnack(
-              t(
-                'documentManagement.snack.noDocsInSelection',
-                'No documents in folder'
-              ),
-              'error'
-            );
-            return;
-          }
-
-          await api.downloadAsZip(docsForZip, doc.name);
+        await api.downloadAsZip(docsForZip, doc.name);
 
         showSnack(
           t('documentManagement.snack.downloaded', 'Download started'),
